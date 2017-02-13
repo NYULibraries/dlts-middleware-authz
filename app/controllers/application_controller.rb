ad464f997523a1f78a86e8c3ee9a1f4ff25d9815
Alex Piechowski <alex@powercode.com>
2017-02-13 11:16:09 -0600

    Implement oauth2 using omniauth-nyulibraries
    It also includes erb->haml conversion which doesn't matter
    The method needs to be moved out of the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authentication_callback
+    auth = request.env['omniauth.auth']
+    session[:auth] = auth
+    redirect_to '/' # TODO: make this redirect back to the asset, or a denied page if needed?
+  end
+
+  def authenticate!
+    redirect_to '/auth/nyulibraries' unless session[:auth] && (session[:auth]['credentials']['expires'] == false || session[:auth]['credentials']['expires_at'] > Time.now.to_i)
+  end
 end

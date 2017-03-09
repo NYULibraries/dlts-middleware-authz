c4b6345c73010ea60c622e96ec98feb976929c62
Alex Piechowski <alex@piechowski.org>
2017-03-09 14:51:35 -0600

    Allow for string interpolation of the uri

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,19 @@ class ItemsController < ApplicationController
 
   def fetch
     end_service = @item.end_service
+
     uri = URI(end_service.uri)
-    uri.query = {item_token: @item.token, session_token: session[:session]['token']}.to_query
+    query = Hash.new
+    {item_token: @item.token, session_token: session[:session]['token']}.each do |url_param, value|
+      if uri.path.include? ":#{url_param}"
+        uri.path = uri.path.gsub ":#{url_param}", value
+      else
+        query[url_param] = value
+      end
+    end
+
+    uri.query = query.to_query unless query.empty?
+
     redirect_to uri.to_s
   end
 
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Items', type: :request do
   describe 'get /items' do
+    before do
+      get "/item/#{item.token}"
+      # follow 3 redirects to auth and come back
+      3.times { follow_redirect! }
+    end
+
     context 'It redirects to the end_service URI with the proper params' do
       let (:item) { FactoryGirl.create(:item) }
       let (:end_service) { item.end_service }
-
-      before do
-        get "/item/#{item.token}"
-        # follow 3 redirects to auth and come back
-        3.times { follow_redirect! }
-      end
 
       it { expect(response).to have_http_status(:redirect) }
       it do
         uri = URI(end_service.uri)
         uri.query = {item_token: item.token, session_token: session[:session]['token']}.to_query
         expect(response).to redirect_to(uri.to_s)
+      end
+    end
+
+    context 'It interpolates strings if it can' do
+      let (:end_service) { FactoryGirl.create(:end_service, uri: 'https://example.com/:session_token/:item_token') }
+      let (:item) { FactoryGirl.create(:item, end_service: end_service) }
+
+      it do
+        expect(response).to redirect_to("https://example.com/#{session[:session]['token']}/#{item.token}")
       end
     end
   end
